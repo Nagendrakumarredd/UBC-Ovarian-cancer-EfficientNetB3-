2bb78c6bbb4817b8b1c2ed79a7317bbc4f497a56
Syamala Nagendra Kumar Reddy <106234201+Nagendrakumarredd@users.noreply.github.com>
2024-06-01 22:22:31 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,3 +2,7 @@ import numpy as np # linear algebra
 import pandas as pd
 There are two categories of images: whole slide images (WSI) and tissue microarray (TMA).
 Whole slide images are at 20x magnification and can be quite large. The TMAs are smaller (roughly 4,000x4,000 pixels) but at 40x magnification.
+
+train=pd.read_csv("/kaggle/input/UBC-OCEAN/train.csv")
+print(train.head())
+print(train.shape)
