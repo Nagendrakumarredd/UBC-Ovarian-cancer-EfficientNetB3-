e313b8991db0c1ffe95fb87f484d7aa5c562e2b7
Syamala Nagendra Kumar Reddy <106234201+Nagendrakumarredd@users.noreply.github.com>
2024-06-01 22:19:32 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,4 @@
+import numpy as np # linear algebra
+import pandas as pd
+There are two categories of images: whole slide images (WSI) and tissue microarray (TMA).
+Whole slide images are at 20x magnification and can be quite large. The TMAs are smaller (roughly 4,000x4,000 pixels) but at 40x magnification.
